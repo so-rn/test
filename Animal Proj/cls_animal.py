38cbbb26fce258f32a347f23d71ca9037f239494
soran <soraan.mahmoudi@gmail.com>
2026-05-11 14:43:02 +0200

    Updated

--- Animal Proj/cls_animal.py
+++
@@ -1,7 +0,0 @@
-class Animal:
-    def __init__(self, name, color):
-        self.name = name
-        self.color = color
-
-    def introduce(self):
-        print(f"Name: {self.name}, Color: {self.color}")
